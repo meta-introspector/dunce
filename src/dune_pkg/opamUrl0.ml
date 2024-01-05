312d17e549d96eed62884a53ad187c4b267bc8c7
Rudi Grinberg <me@rgrinberg.com>
2024-01-05 12:08:46 -0700

    fix(pkg): pass locations to [Opam_repo.Source] and to command line
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>
    
    <!-- ps-id: 86dba9f9-5d03-4cd2-92b9-a2980dfbdacb -->

--- src/dune_pkg/opamUrl0.ml
+++ src/dune_pkg/opamUrl0.ml
@@ -3,9 +3,9 @@ open Stdune
 
 let decode =
   let open Dune_sexp.Decoder in
-  map_validate string ~f:(fun s ->
+  map_validate (located string) ~f:(fun (loc, s) ->
     match OpamUrl.of_string s with
-    | url -> Ok url
+    | url -> Ok (loc, url)
     | exception OpamUrl.Parse_error m ->
       Error (User_message.make [ Pp.text "invalid url "; Pp.text m ]))
 ;;
